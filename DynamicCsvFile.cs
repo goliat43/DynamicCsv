adb5165a8fabdb455567fa7110aa019722f47167
niclase <niclas.eriksson@knowit.se>
2017-12-19 14:57:29 +0100

    First content!

---
+++ DynamicCsvFile.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DynamicCsv
+{
+    public static class Constants
+    {
+        public const char Separator = ',';
+    }
+
+    /// <summary>
+    /// Property of Niclas Eriksson 
+    /// </summary>
+    public sealed class DynamicCsvFile : IEnumerable<DynamicCsvRow>
+    {
+        private int count;
+
+        private DynamicCsvFile()
+        {
+        }
+
+        public List<string> Headers { get; } = new List<string>();
+        public List<DynamicCsvRow> Rows { get; } = new List<DynamicCsvRow>();
+
+        public int Count => count;
+
+        public static DynamicCsvFile Load(StreamReader filestream)
+        {
+            if (filestream == null) throw new ArgumentNullException(nameof(filestream));
+
+            var csvFile = new DynamicCsvFile();
+            string row;
+
+            //Read the header and store it.
+            string header = filestream.ReadLine();
+            if (string.IsNullOrWhiteSpace(header)) throw new InvalidDataException("Header in CSV was empty");
+            csvFile.Headers.AddRange(header.Split(Constants.Separator));
+
+            //Read the rest of the file.
+            while ((row = filestream.ReadLine()) != null)
+            {
+                csvFile.count++; //TODO: Ugly!
+                //Skip empty rows.
+                if (string.IsNullOrWhiteSpace(row)) continue;
+
+                csvFile.Rows.Add(new DynamicCsvRow(row, csvFile.Headers));
+            }
+
+            return csvFile;
+        }
+
+        public IEnumerator<DynamicCsvRow> GetEnumerator()
+        {
+            return Rows.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
+
---
+++ DynamicCsvRow.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace DynamicCsv
+{
+    [System.Diagnostics.DebuggerDisplay("{ToDebugString()}")]
+    public sealed class DynamicCsvRow : DynamicObject, IEnumerable<string>
+    {
+        private readonly List<string> header;
+        private readonly List<string> row;
+        public dynamic Dynamic => this;
+
+        public DynamicCsvRow(string rowContent, List<string> header)
+        {
+            if (rowContent == null) throw new ArgumentNullException(nameof(rowContent));
+            if (header == null) throw new ArgumentNullException(nameof(header));
+
+            this.header = header;
+            this.row = rowContent.Split(Constants.Separator).ToList();
+
+            if (header.Count != row.Count) throw new InvalidDataException("CSV row content doesn't match header.");
+        }
+
+
+        #region Dynamic
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            var indexToGet = header.IndexOf(binder.Name);
+
+            result = null;
+            if (indexToGet == -1) return false;
+
+            result = row[indexToGet];
+
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            var indexToSet = header.IndexOf(binder.Name);
+            if (indexToSet == -1) return false;
+
+            row[indexToSet] = value.ToString();
+            return true;
+        }
+
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            result = null;
+
+            if (indexes[0] == null) return false;
+
+            if (indexes[0] is int)
+            {
+                var index = (int)indexes[0];
+                result = row[index];
+                return true;
+            }
+            if (indexes[0] is string)
+            {
+                var att = (string)indexes[0];
+                var index = header.IndexOf(att);
+                result = row[index];
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object value)
+        {
+            if (indexes[0] == null) return false;
+
+            if (indexes[0] is int)
+            {
+                var index = (int)indexes[0];
+                row[index] = value.ToString();
+
+            }
+            else if (indexes[0] is string)
+            {
+                var att = (string)indexes[0];
+                var index = header.IndexOf(att);
+                if (index == -1) return false;
+
+                row[index] = value.ToString();
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public override bool TryConvert(ConvertBinder binder, out object result)
+        {
+            result = null;
+            if (binder.Type == typeof(List<string>))
+            {
+                result = row;
+                return true;
+            }
+            if (binder.Type == typeof(string))
+            {
+                result = string.Join(Constants.Separator.ToString(), row);
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            var type = typeof(List<string>);
+            try
+            {
+                result = type.InvokeMember(binder.Name,
+                                           BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Instance,
+                                           null, row, args);
+
+                return true;
+            }
+            catch
+            {
+                result = null;
+                return false;
+            }
+        }
+
+        #endregion Dynamic
+
+
+        #region Enumerable
+        public IEnumerator<string> GetEnumerator()
+        {
+            return ((IEnumerable<string>)row).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerable<dynamic> AsDynamicEnumerable()
+        {
+            return this;
+        }
+        #endregion Enumerable
+
+
+        public string ToDebugString()
+        {
+            if (row == null || row.Count == 0) return string.Empty;
+
+            var sb = new string[header.Count];
+            for (var i = 0; i < header.Count; i++)
+            {
+                sb[i] = $"{header[i]}={row[i]}";
+            }
+
+            return string.Join(Constants.Separator.ToString(), sb);
+        }
+
+        public override string ToString()
+        {
+            return (row != null && row.Count > 0) ? string.Join(Constants.Separator.ToString(), row) : string.Empty;
+        }
+    }
+}
